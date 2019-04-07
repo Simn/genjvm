ff06af07ba4758f82d194435316a85016ef86975
Simon Krajewski <simon@haxe.org>
2019-04-07 21:12:28 +0200

    build with debug

--- tests/genjvm/debug.bat
+++ tests/genjvm/debug.bat
@@ -1,1 +1,1 @@
-unzip -o export/Main.jar -d export && javap -v -c -s -constants export/Main.class > export/Main.txt
+unzip -o export/Main-debug.jar -d export && javap -v -c -s -constants export/Main.class > export/Main.txt
