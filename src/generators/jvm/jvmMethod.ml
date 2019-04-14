4868d65b9d0fbb6decbadfe931c7dbf0409c805a
Simon Krajewski <simon@haxe.org>
2019-04-14 11:10:01 +0200

    add commented out adapt_method method
    There are some problems with unboxing...

--- src/generators/jvm/jvmMethod.ml
+++ src/generators/jvm/jvmMethod.ml
@@ -357,6 +357,20 @@ class builder jc name jsig = object(self)
 			| _ -> ()
 		end
 
+	method adapt_method jsig =
+		()
+		(* let offset = code#get_pool#add_string (generate_method_signature false jsig) in
+		let offset = code#get_pool#add (ConstMethodType offset) in
+		self#get_code#dup;
+		self#if_then
+			(fun () -> self#get_code#if_null_ref jsig)
+			(fun () ->
+				code#ldc offset method_type_sig;
+				self#invokevirtual method_handle_path "asType" (method_sig [method_type_sig] (Some method_handle_sig))
+			);
+		ignore(code#get_stack#pop);
+		code#get_stack#push jsig; *)
+
 	(** Casts the top of the stack to [jsig]. If [allow_to_string] is true, Jvm.toString is called. **)
 	method cast ?(not_null=false) ?(allow_to_string=false) jsig =
 		let jsig' = code#get_stack#top in
@@ -425,12 +439,13 @@ class builder jc name jsig = object(self)
 		| TObject(path,_),TTypeParameter _ ->
 			code#checkcast path
 		| TMethod _,TMethod _ ->
-			()
-		| TMethod _,TObject((["java";"lang";"invoke"],"MethodHandle"),_)
+			if jsig <> jsig' then self#adapt_method jsig;
+		| TMethod _,TObject((["java";"lang";"invoke"],"MethodHandle"),_) ->
+			self#adapt_method jsig;
 		| TObject((["java";"lang";"invoke"],"MethodHandle"),_),TMethod _ ->
 			()
 		| TMethod _,_ ->
-			code#checkcast (["java";"lang";"invoke"],"MethodHandle")
+			code#checkcast (["java";"lang";"invoke"],"MethodHandle");
 		| TArray(jsig1,_),TArray(jsig2,_) when jsig1 = jsig2 ->
 			()
 		| TArray _,_ ->
